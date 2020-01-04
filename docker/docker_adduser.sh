ab306de4f81d15a7b28a1d3e4a3d558656844df7
fengzongbao <mickeyouou@gmail.com>
2020-01-04 16:38:07 +0800

    docker: fix docker user add bug

--- docker/docker_adduser.sh
+++ docker/docker_adduser.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 addgroup --gid "$DOCKER_GRP_ID" "$DOCKER_GRP"
-adduser --disabled-password --force-badname --gecos '' "$DOCKER_USER" \
-    --uid "$DOCKER_USER_ID" --gid "$DOCKER_GRP_ID" 2>/dev/null
+adduser --disabled-password --force-badname --gecos '' "$DOCKER_USER"
 usermod -aG sudo "$DOCKER_USER"
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 cp -r /etc/skel/. /home/${DOCKER_USER}
